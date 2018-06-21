47d47fb6ff6795242cdbc54e51732b4d34269404
wangsheng <wangsheng6985@gmail.com>
2018-06-21 07:09:12 +0000

    Nikola auto commit.
    Nikola version: 7.8.15

--- conf.py
+++ conf.py
@@ -142,7 +142,7 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "maupassant"
+THEME = "foundation6"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
